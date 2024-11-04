7469eb05b0371594e376d0a9ce1547ad816a72cd
jnollen13 <jerome.nollen@gmail.com>
2024-11-04 12:15:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairNorth, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level1`)
+})
+game.splash("welcome to the arena")
+game.splash("in this game ", "you are trying to advance and complete all 9 levels")
+game.splash("and become the champion of the arena")
+tiles.setCurrentTilemap(tilemap`barracks`)
+let mySprite = sprites.create(assets.image`Jodim`, SpriteKind.Player)
+controller.moveSprite(mySprite, 102, 102)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.floorLight0)
+scene.cameraFollowSprite(mySprite)
